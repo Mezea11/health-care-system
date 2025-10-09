9c8098bf5aaf5cbca23a394a4050bb06502dca45
Mezea11 <christiancastellanosmeza@gmail.com>
2025-10-09 14:44:12 +0200

    added breaks in comments for better readability

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@
 As a user, I need to be able to log in. CHECKED
 As a user, I need to be able to log out.
 As a user, I need to be able to request registration as a patient.
+
+
 As an admin with sufficient permissions, I need to be able to give admins the permission to handle the permission system, in fine granularity.
 As an admin with sufficient permissions, I need to be able to assign admins to certain regions.
 As an admin with sufficient permissions, I need to be able to give admins the permission to handle registrations.
@@ -18,12 +20,16 @@ As an admin with sufficient permissions, I need to be able to accept user regist
 As an admin with sufficient permissions, I need to be able to deny user registration as patients.
 As an admin with sufficient permissions, I need to be able to create accounts for personnel.
 As an admin with sufficient permissions, I need to be able to view a list of who has permission to what.
+
+
 As personnel with sufficient permissions, I need to be able to view a patients journal entries.
 As personnel with sufficient permissions, I need to be able to mark journal entries with different levels of read permissions.
 As personnel with sufficient permissions, I need to be able to register appointments.
 As personnel with sufficient permissions, I need to be able to modify appointments.
 As personnel with sufficient permissions, I need to be able to approve appointment requests.
 As personnel with sufficient permissions, I need to be able to view the schedule of a location.
+
+
 As a patient, I need to be able to view my own journal.
 As a patient, I need to be able to request an appointment.
 As a logged in user, I need to be able to view my schedule.
