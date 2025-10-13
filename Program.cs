635016b8670a56a0cbd92c12519478238d9da8c7
jarl <jarllindquist@gmail.com>
2025-10-13 10:24:59 +0200

    bug fix: Forgot to break the if statement

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using App;
 
 As a user, I need to be able to log in. CHECKED
 As a user, I need to be able to log out. CHECKED
-As a user, I need to be able to REQUEST registration as a patient.
+As a user, I need to be able to REQUEST registration as a patient. CHECKED 
 
 
 As an admin with sufficient permissions, I need to be able to give admins the permission to handle the permission system, in fine granularity.
@@ -55,7 +55,6 @@ List<IUser> users = new List<IUser>()
                 new User("patient", "123", Role.Patient),
                 new User("personell", "123", Role.Personnel),
                 new User("admin", "123", Role.Admin)
-
             };
 IUser? activeUser = null;
 bool running = true;
@@ -76,12 +75,12 @@ void StartMenu(List<IUser> users)
         Console.WriteLine("Welcome");
         Console.WriteLine("1. For sending request for registration as patient");
         Console.WriteLine("2. Log In");
-        Console.Write("Choice: ");
-        string choice = Console.ReadLine();
+        // Console.Write("Choice: ");
+        // string choice = Console.ReadLine();
 
-        switch (choice)
+        switch (Utils.GetIntegerInput("Choice a number: "))
         {
-            case "1":
+            case 1:
                 // CREATE LOGIC IN HERE TO REGISTER A NEW USER
                 Console.WriteLine("Type in your username");// PROMPT USER TO INSERT USERNAME
                 string newUser = Console.ReadLine();
@@ -96,7 +95,7 @@ void StartMenu(List<IUser> users)
                 users.Add(new User(newUser, newPass, Role.Patient));  // CREATE NEW OBJECT (WITH ROLE PATIENT) WITH USERNAME AND PASSWORD
                 break;
 
-            case "2":
+            case 2:
                 MainMenu();
                 break;
         }
@@ -152,6 +151,7 @@ void MainMenu()
                 Utils.DisplayAlertText("Your account is still pending. Need to wait for the admin to accept it, Press ENTER to continue");
                 Console.ReadKey();
                 activeUser = null;
+                break;
             }
             // ============================
             // MENU: LOGGED IN
